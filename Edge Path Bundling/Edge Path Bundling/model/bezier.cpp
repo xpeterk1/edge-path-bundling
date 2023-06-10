df848da7c6c4699854231a27d44caa3977d2eeb9
joergchristian <e12213117@student.tuwien.ac.at>
2023-06-10 16:59:08 +0200

    bezier on sphere added
    Also ads glm
    BUG: glm:rotate does not work!!!

--- Edge Path Bundling/Edge Path Bundling/model/bezier.cpp
+++ Edge Path Bundling/Edge Path Bundling/model/bezier.cpp
@@ -1,7 +1,8 @@
 #pragma once
 #include "bezier.h"
 
-Coordinates bezierFlat(const std::vector<Coordinates>& controlPoints, double t) {
+Coordinates bezierFlat(const std::vector<Coordinates>& controlPoints, float t) 
+{
     // Check if we have enough control points
     if (controlPoints.size() < 2)
     {
@@ -29,4 +30,51 @@ Coordinates bezierFlat(const std::vector<Coordinates>& controlPoints, double t) 
         points = std::move(intermediatePoints);
     }
     return points[0];
+}
+
+glm::vec3 bezierSphere(const std::vector<Coordinates>& controlPoints, float t, float radius)
+{
+    // Check if we have enough control points
+    if (controlPoints.size() < 2)
+    {
+        std::cerr << "\nError: Less than 2 ctrl points" << std::endl;
+        return glm::vec3(0,0,0);
+    }
+    if (t < 0 or t>1)
+    {
+        std::cerr << "\nError: t not in [0,1]" << std::endl;
+        return glm::vec3(0, 0, 0);
+    }
+    
+    // covert long/lat control points to 3d vectors
+    std::vector<glm::vec3> points;
+    for(Coordinates point : controlPoints)
+    {
+        float phi = point.longitude;
+        float theta = 90.0f - point.latitude;
+        glm::vec3 currentVector(std::sin(theta) * std::cos(phi), std::sin(theta) * std::sin(phi), std::cos(theta));
+        points.push_back(radius * currentVector);
+    }
+
+    // Calculate the intermediate points
+    while(points.size() > 1)
+    {
+        std::vector<glm::vec3> intermediatePoints;
+        for (size_t i = 0; i < points.size() - 1; ++i) 
+        {
+            glm::vec3 vector1 = points[i];
+            glm::vec3 vector2 = points[i + 1];
+            glm::vec3 n = glm::cross(vector1, vector2);
+            float cosAlpha = glm::dot(vector1, vector2);
+            float alphaRad = glm::acos(cosAlpha);
+            float betaRad= t * alphaRad; 
+            glm::mat4 rotationMat = glm::rotate(betaRad, n);
+            glm::vec4 result4d = rotationMat * glm::vec4(vector1,1);
+            glm::vec3 result(result4d.x, result4d.y, result4d.z);
+            intermediatePoints.push_back(result);
+        }
+        points = std::move(intermediatePoints);
+    }
+    return points[0];
+
 }
--- Edge Path Bundling/Edge Path Bundling/model/bezier.h
+++ Edge Path Bundling/Edge Path Bundling/model/bezier.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <math.h>
+#include "glm/glm.hpp" 
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtc/constants.hpp"
+
 
 struct Coordinates 
 {
@@ -8,7 +14,9 @@ struct Coordinates
     double latitude;
 };
 
+
 //Returns 2d coordinates in the plane of a point on a bezier curve specified by t in [0,1]
-Coordinates bezierFlat(const std::vector<Coordinates>& controlPoints, double t);
+Coordinates bezierFlat(const std::vector<Coordinates>& controlPoints, float t);
 
 // TODO: write method that calculates bezier curve on sphere
+glm::vec3 bezierSphere(const std::vector<Coordinates>& controlPoints, float t, float radius = 1);
