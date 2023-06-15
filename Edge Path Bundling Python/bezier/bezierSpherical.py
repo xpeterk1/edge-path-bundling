a3a7ec63b0140830daceaf26e62d0694f1f6203b
joergchristian <e12213117@student.tuwien.ac.at>
2023-06-15 20:51:00 +0200

    drawing boundaries again

--- Edge Path Bundling Python/bezier/bezierSpherical.py
+++ Edge Path Bundling Python/bezier/bezierSpherical.py
@@ -222,11 +222,15 @@ def plot_spherical(control_point_lists, nodes, edges, n=-1, step_size=1):
             y = [arr[1] for arr in segment.tolist()]
             plt.plot(x, y, color='blue', linewidth=0.1,  alpha=1)
 
+    # draw boundaries of texture
+    boundaries = [[-180, -90], [-180, 90], [180, 90], [180, -90], [-180, -90]]
+    x, y = zip(*boundaries)
+    plt.plot(x, y, color='black', linewidth=0.1,  alpha=1)
 
     # plt.axis('scaled')
     ax = plt.gca()
     ax.set_aspect('equal', adjustable='box')
-    plt.axis('Off')
+    plt.axis('On')
     #plt.gcf().set_dpi(300)
     plt.tight_layout()
 
